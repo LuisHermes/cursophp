9bb881a18e5fd90d60b5623236962e2931f7b39b
luishermes <luishenriquefrance@gmail.com>
2024-02-06 13:10:31 +0100

    php Moderno

---
+++ ex008/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Soma</title>
+</head>
+<body>
+    <?php 
+        $res = 50 / 2 + 3 ** 2; // Se tiver algum numero entre parenteses , resolve oque esta em parenteses primeiro , se nao o resultado muda se começa do primeiro numero , se por acasso tiver parenteses no meio da conta .
+        echo "O resultado é $res";
+        
+
+    ?>
+    
+</body>
+</html>
