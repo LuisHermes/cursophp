f77c82e7425141a5a6c4f5fb0a7c13a39cdeefa4
luishermes <luishenriquefrance@gmail.com>
2024-02-07 14:33:55 +0100

    Desafios php

---
+++ Desafios php/Desafio002/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Desafio 002</title>
+</head>
+<body>
+    <main>
+        <h1>Gerador numeros aleatorios</h1>
+        <?php 
+            $min = 0;
+            $max = 50;
+            $resultado = mt_rand($min,$max);
+
+            echo "O Numero aleatorio é  : " , $resultado;
+        ?>
+        <button onclick="javascript:document.location.reload()">Gerar outro</button>
+    </main>
+    
+</body>
+</html>
